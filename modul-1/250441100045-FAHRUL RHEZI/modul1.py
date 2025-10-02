470fd9d12282ded702f88b35d61df722430a1b2f
afifutorif <torifafifu@gmail.com>
2025-10-02 21:59:28 +0700

    250441100045-FahrulRhezi-AsprakKakAinunSofia-Modul1

--- modul-1/250441100045-FAHRUL RHEZI/modul1.py
+++ modul-1/250441100045-FAHRUL RHEZI/modul1.py
@@ -1,30 +1,30 @@
-# #Modul 1 Praktikum
-# #Soal 1
-# harga_buku_tulis = 5000
-# harga_pensil = 4500
-# pajak = 0.10
-# j_buku_tulis = 3
-# j_pensil = 2
+#Modul 1 Praktikum
+#Soal 1
+harga_buku_tulis = 5000
+harga_pensil = 4500
+pajak = 0.10
+j_buku_tulis = 3
+j_pensil = 2
 
-# j_harga_buku_tulis = harga_buku_tulis * j_buku_tulis
-# j_harga_pensil = harga_pensil * j_pensil
-# total_biaya = j_harga_buku_tulis + j_harga_pensil
-# biaya_pajak = total_biaya * pajak
-# setelah_pajak = total_biaya + biaya_pajak
+j_harga_buku_tulis = harga_buku_tulis * j_buku_tulis
+j_harga_pensil = harga_pensil * j_pensil
+total_biaya = j_harga_buku_tulis + j_harga_pensil
+biaya_pajak = total_biaya * pajak
+setelah_pajak = total_biaya + biaya_pajak
 
-# print(f"total biaya: Rp.{total_biaya}")
-# print(f"pajak 10%: Rp.{biaya_pajak}")
-# print(f"total biaya : Rp.{setelah_pajak}")
+print(f"total biaya: Rp.{total_biaya}")
+print(f"pajak 10%: Rp.{biaya_pajak}")
+print(f"total biaya : Rp.{setelah_pajak}")
 
-# #Soal 2
-# panjang = int(input("Masukkan panjang : "))   #10
-# lebar = int(input("Masukkan lebar : "))    #6  
-# tinggi = int(input("Masukkan tinggi : "))  #4
+#Soal 2
+panjang = int(input("Masukkan panjang : "))   #10
+lebar = int(input("Masukkan lebar : "))    #6  
+tinggi = int(input("Masukkan tinggi : "))  #4
 
-# volume = panjang * lebar * tinggi
-# luas_permukaan = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi)
-# print(f"Volume Balok : {volume}cm³")
-# print(f"Luas Permukaan Balok : {luas_permukaan}cm")
+volume = panjang * lebar * tinggi
+luas_permukaan = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi)
+print(f"Volume Balok : {volume}cm³")
+print(f"Luas Permukaan Balok : {luas_permukaan}cm")
 
 #Soal 3
 bola_merah = int(input("masukkan jumlah bola merah: "))   #8
