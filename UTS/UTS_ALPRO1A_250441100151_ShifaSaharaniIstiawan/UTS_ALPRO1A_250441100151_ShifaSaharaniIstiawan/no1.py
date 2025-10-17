8a3e8a1a16a5fbd2dc24fc52736f5424055d7523
Shifashrn <shifasaaahrn1@gmail.com>
2025-10-17 16:18:40 +0700

    250441100151_ShifaSaharaniIstiawan_UTS_ASPRAKKAKHEEMA

---
+++ UTS/UTS_ALPRO1A_250441100151_ShifaSaharaniIstiawan/UTS_ALPRO1A_250441100151_ShifaSaharaniIstiawan/no1.py
@@ -0,0 +1,10 @@
+#Program rental motor Aconk
+
+hargasewamotormatic : 50000
+hargasewamotortrail : 100000
+hargasewamotorsport : 75000
+
+n = int(input(" pilihan jenis motor"),n, +1, ":")
+n =(float("harga motor tiap jenis"), n, +1, ":")
+n =(float("asuransi jika lebih dari 3 hari penyewaaan"))
+    print : 10 ** 100
