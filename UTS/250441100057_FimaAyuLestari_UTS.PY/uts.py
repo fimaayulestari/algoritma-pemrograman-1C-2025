800ee761eae78e25516e9001579629815f482b91
fima ayu lestari <fimaayulestari04@gmail.com>
2025-10-17 15:35:06 +0700

    250441100057_FimaAyuLestari_UTS_AsprakKakAinun

---
+++ UTS/250441100057_FimaAyuLestari_UTS.PY/uts.py
@@ -0,0 +1,36 @@
+jenis_motor = input("motor matic, motor trail, motor sport")
+
+motor_matic = int(input("masukkan harga motor matic: "))
+motor_trail = int(input("masukkan harga motor trail: "))
+motor_trail = int(input("masukkan harga motor sport: "))
+
+sewa_lebih = 3
+asuransi = 15000
+
+
+while sewa_lebih > 0:
+    if i == 3:
+        print("tidak dapat asuransi")
+    else:
+        print("dapat asuransi")
+
+diskon
+
+harga = 
+
+# asuransi = 15000
+
+# ulang = "y"
+
+# sewa_lebih = 3
+
+# while ulang > 0:
+#     if i > 0:
+#         ulang += 1
+# elif sewa_lebih:
+#     if i > 0:
+#         print("anda mendapatkan asuransi=", asuransi)
+
+# while asuransi > 15:
+
+
