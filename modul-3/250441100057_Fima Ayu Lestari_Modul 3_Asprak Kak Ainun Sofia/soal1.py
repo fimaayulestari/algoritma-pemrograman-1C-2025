b719e9587e2cb294c9bc16fd8e53263c7883ce1e
fima ayu lestari <fimaayulestari04@gmail.com>
2025-10-30 17:47:24 +0700

    250441100057_FimaAyuLestari_Modul4_AsprakKakAinunSofia

--- modul-3/250441100057_Fima Ayu Lestari_Modul 3_Asprak Kak Ainun Sofia/soal1.py
+++ modul-3/250441100057_Fima Ayu Lestari_Modul 3_Asprak Kak Ainun Sofia/soal1.py
@@ -12,38 +12,3 @@
 #             break
 #     if prima:
 #         print(angka)
-
-# for i in range(1, 4):
-#     for j in range(1, 4):
-#         print(i, j)
-
-# for i in range(1, 4):        # Perulangan luar
-#     for j in range(1, 4):    # Perulangan dalam
-#         print(j)
-
-# data = [1, 2, 3, 4, 5]
-
-# for i in data:
-#     for j in data:
-#         if i != j:
-#             print(i, j)
-
-a, b = 0, 1
-for i in range(10):
-    print(a)
-    a, b = b, a + b
-
-# Program pola piramida angka sederhana
-
-baris = int(input("Masukkan jumlah baris: "))
-
-for i in range(1, baris + 1):
-    # Cetak spasi biar bentuknya seperti piramida
-    print(" " * (baris - i), end="")
-    
-    # Cetak angka naik di setiap baris
-    for j in range(1, i + 1):
-        print(j, end=" ")
-    
-    # Pindah ke baris baru
-    print()
